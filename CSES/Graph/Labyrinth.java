a0c7b3eca1afa7815fa8bd4c953bd2a2c5153726
Mahima Singh <58841158+with-mahimasingh@users.noreply.github.com>
2021-12-02 17:05:47 +0530

    1193 Labyrinth

--- CSES/Graph/Labyrinth.java
+++ CSES/Graph/Labyrinth.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class CSESGraph {
-
+ 
     static class Pair {
         int x;
         int y;
-
+ 
         Pair(int x, int y)
         {
             this.x = x;
@@ -18,11 +18,11 @@ public class CSESGraph {
     static boolean[][] vis= new boolean[1001][1001];
     static int n , m;
     static ArrayList<Character> path= new ArrayList<>();
-
+ 
     static boolean isValid(int i, int j){
-
+ 
         if(i < 0 || i >= n || j < 0 || j >=m || ar[i][j] == '#' || vis[i][j]) return false;
-
+ 
         return true;
     }
     static boolean bfs(int x , int y)
@@ -30,21 +30,21 @@ public class CSESGraph {
         Queue<Pair> q= new LinkedList<>();
         q.add(new Pair(x , y));
         vis[x][y] = true;
-
+ 
         while(!q.isEmpty()){
             int a = q.peek().x;
             int b = q.peek().y;
-
+ 
             q.poll();
             if(ar[a][b] == 'B'){
                 while(true){
                     path.add(br[a][b]);
-
+ 
                     if(path.get(path.size()-1) == 'L') b++;
                     if(path.get(path.size()-1) == 'R') b--;
                     if(path.get(path.size()-1) == 'U') a++;
                     if(path.get(path.size()-1) == 'D') a--;
-
+ 
                     if(a == x && b == y)
                         break;
                 }
@@ -56,15 +56,15 @@ public class CSESGraph {
                 q.add(new Pair(a , b-1));
                 vis[a][b-1] = true;
             }
-
-
+ 
+ 
             //right
             if(isValid(a , b + 1)) {
                 br[a][b+1] = 'R' ;
                 q.add(new Pair(a , b+1));
                 vis[a][b+1] = true;
             }
-
+ 
             //up
             if(isValid(a-1 , b )) {
                 br[a-1][b] = 'U' ;
@@ -80,16 +80,18 @@ public class CSESGraph {
         }
         return false;
     }
-
-
+ 
+ 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-         n = sc.nextInt();
-         m = sc.nextInt();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+         n = Integer.parseInt(st.nextToken());
+         m = Integer.parseInt(st.nextToken());
+ 
         int p=0,q = 0;
         for (int i = 0; i < n; i++) {
-            String s= sc.next();
+            String s = br.readLine();
             for (int j = 0; j < m; j++)
             {
                 ar[i][j]=s.charAt(j);
@@ -100,13 +102,14 @@ public class CSESGraph {
             }
         }
         if(bfs(p, q)){
-            System.out.println("YES \n"+ path.size());
+            out.println("YES \n"+ path.size());
             int size= path.size()-1;
             while(size>=0)
-                System.out.print(path.get(size--));
+                out.print(path.get(size--));
         }
         else{
-            System.out.println("NO");
+            out.println("NO");
         }
+        out.close();
     }
 }
